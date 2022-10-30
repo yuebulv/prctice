8b107a1cb5e94de025ab47fb4b022cfa84cfcf85
yuebu_global <yuebu_global@qq.com>
2022-10-31 00:10:05 +0800

    excel 数据清洗

---
+++ Road/excel_py/excel_sql.py
@@ -0,0 +1,25 @@
+"""
+目标：实现excel <--> mysql 双向便捷交互
+
+# 1。1 excel_to_excel
+  - get_tf_km_age(项目名称，路线，data_source=[excel_name,mysql], axis="返回值轴向") -> [DataFrame,Series]
+  - get_tf_km_data(项目名称，路线，起点，止点，data_source=[excel_name,mysql], axis="返回值轴向") -> [DataFrame,Series]
+    - return:挖方 【土方（1，2，3类），石方（4,5,6类）】；填方 【土方（1，2，3类），石方（4,5,6类）】
+            本桩利用
+            远运利用
+            借方
+            废方
+    class sheet_road():
+        sheet_tf_km
+        sheet_bridgehead_roadbed
+        sheet_mud_base
+        sheet_protect
+        sheet_drain
+        sheet_
+    class road()
+    起点、止点、左幅、右幅、整幅、长度
+
+## 1.2 mysql_to_excel
+# 2.to_mysql
+
+"""
--- Road/excel_py/test.py
+++ Road/excel_py/test.py
@@ -10,20 +10,56 @@ def test(path):
 
 
 if __name__ == "__main__":
-    excel_path = r"E:\code\notes\noteOnGithub\data\SZYS06010111 每公里土石方数量表汇总表.xls"
+    # excel_path = r"E:\code\notes\noteOnGithub\data\SZYS06010111 每公里土石方数量表汇总表.xls"
+    # toexcel_path = r"E:\code\notes\noteOnGithub\data\toexcel.xlsx"
+    excel_path = r"D:\lvcode\noteOnGithub\noteOnGithub\data\SZYS06010111 每公里土石方数量表汇总表.xls"
+    toexcel_path = r"D:\lvcode\noteOnGithub\noteOnGithub\data\toexcel.xlsx"
     sheetName = "黄阁西互通"
     skiprows_sheet = 2
-    toexcel_path = r"E:\code\notes\noteOnGithub\data\toexcel.xlsx"
+    data_range_in_excel = [[0, 50], [0, 45]]
     dataDf: DataFrame
     dataDf = pd.read_excel(excel_path, sheet_name=sheetName, skiprows=skiprows_sheet, na_filter=0, header=[0, 1, 2, 3])
-    print(dataDf.head())
+    # dataDf = pd.read_excel(excel_path, sheet_name=sheetName, skiprows=skiprows_sheet, na_filter=0)
+    # print(dataDf.head())
     pd.options.display.max_columns = 10
-    print(dataDf.shape)
+    # dataDf.dropna(axis=1, how="all", inplace=True)
+    # dataDf.dropna(axis=0, how="all", inplace=True)
+    # print(dataDf.info)
+    dataDf = dataDf.iloc[data_range_in_excel[0][0]:data_range_in_excel[0][1], data_range_in_excel[1][0]:data_range_in_excel[1][1]]
+    # print(dataDf.head())
+
+    # 索引清洗
+    mi = dataDf.columns
+    # print(dataDf.columns)
+    mi_df = mi.to_frame(index=False)
+    # print(mi_df)
+    mi_df_shape = mi_df.shape
+    for i in range(0, mi_df_shape[1]):
+        mi_df.loc[:, i] = mi_df.loc[:, i].str.replace(r"[ \n\r\t]", "", regex=True) \
+            .str.replace(r"Unnamed:.+", "", regex=True)
+    # print(mi_df)
+    dataDf.columns = pd.MultiIndex.from_frame(mi_df)
+    # print(dataDf)
+    print(mi_df_shape)
+
+    # 删除空行，空列
+    # for i in range(0, mi_df_shape[0]):
+    #     print(dataDf.iloc[:, i].map(lambda x: x == ""))
+    #     print(f"{i}行:{dataDf.iloc[:, i]}")
+    # print(dataDf)
+    # print(dataDf.apply(lambda x: np.nan if x == "" else x))
+    dataDf.replace("", np.nan, inplace=True)
+    dataDf.replace(0, np.nan, inplace=True)
     dataDf.dropna(axis=1, how="all", inplace=True)
-    print(dataDf.shape)
     dataDf.dropna(axis=0, how="all", inplace=True)
-    print(dataDf.shape)
-    # dataDf.drop(columns=["Unnamed: 51"], inplace=True)
-    dataDf.to_excel(toexcel_path)
-    newDf = dataDf.loc[0:4]
-    print(newDf)
+    # 删除重复行、列
+    dataDf.replace(np.nan, "", inplace=True)  # np.nan == np.nan 返回False
+    dataDf = dataDf.T.drop_duplicates(keep="first").T
+
+    print(dataDf[("挖方", "土方", "普通土")])
+    print(dataDf["起讫桩号"])
+    # print(dataDf.iloc[:, 0], dataDf.iloc[:, 16])
+    # dataDf["result"] = np.where(dataDf.iloc[:, 0] == dataDf.iloc[:, 16], "same", "dif")  # np.nan == np.nan 返回False
+    # print(dataDf["result"])
+    # print(dataDf.iloc[:, 6].map(lambda x:x==""))
+    # dataDf.to_excel(toexcel_path)
